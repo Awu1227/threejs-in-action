a57931fcdb08d0fd35d1937e779d530f293c70f6
Orange <2497436726@qq.com>
2022-11-08 19:48:45 +0800

    :sparkles:basic scene

--- src/script.js
+++ src/script.js
@@ -1,252 +1,40 @@
-import "./style.css";
-import * as THREE from "three";
-import * as dat from "lil-gui";
-import gsap from "gsap";
-import { AdditiveBlending } from "three";
+import './style.css'
+import * as THREE from 'three'
 
-/**
- * Debug
- */
-// const gui = new dat.GUI();
-
-const parameters = {
-  materialColor: "#ffeded",
-};
-
-// gui.addColor(parameters, "materialColor").onChange(() => {
-//   material.color.set(parameters.materialColor);
-// });
-
-/**
- * Base
- */
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-const isPortrait = sizes.width < sizes.height;
-
-let objectsDistance = 5;
+    width: 800,
+    height: 600
+}
 
 /**
  * Camera
  */
-// Base camera
-
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(0, 0, 4);
-// 移动端适配
-if (isPortrait) {
-  camera.position.setZ(8);
-  objectsDistance = 11;
-}
-cameraGroup.add(camera);
-
-const loadingManager = new THREE.LoadingManager();
-loadingManager.onStart = () => {
-  console.log("onStart");
-};
-loadingManager.onProgress = () => {
-  console.log("onProgress");
-};
-loadingManager.onLoad = () => {
-  console.log("onLoad");
-  const loadingEle = document.querySelector("#loading");
-  loadingEle.style.opacity = "0";
-  setTimeout(() => {
-    loadingEle.style.display = "none";
-  }, 300);
-};
-loadingManager.onError = () => {
-  console.log("onError");
-};
-
-/**
- * Objects
- */
-
-// Texture
-const textureLoader = new THREE.TextureLoader(loadingManager);
-const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
-gradientTexture.magFilter = THREE.NearestFilter;
-
-const particleTexture = textureLoader.load("/textures/particles/1.png");
-
-// Material
-const material = new THREE.MeshToonMaterial({
-  color: parameters.materialColor,
-  gradientMap: gradientTexture,
-});
-
-// Meshes
-const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
-const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 23), material);
-const mesh3 = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-  material
-);
-scene.add(mesh1, mesh2, mesh3);
-
-const sectionMeshes = [mesh1, mesh2, mesh3];
-sectionMeshes.forEach((item, index) => {
-  if (isPortrait) {
-    item.position.setY(-objectsDistance * index);
-  } else {
-    item.position.setX(index % 2 === 0 ? -2 : 2);
-    item.position.setY(-objectsDistance * index);
-  }
-});
-/**
- * Particles
- */
-
-const particlesCount = 200;
-const positions = new Float32Array(particlesCount * 3);
-
-for (let i = 0; i < particlesCount; i++) {
-  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
-  positions[i * 3 + 1] =
-    objectsDistance * 0.5 -
-    Math.random() * objectsDistance * sectionMeshes.length;
-  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
-}
-const particlesGeometry = new THREE.BufferGeometry();
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
-
-// Material
-const particlesMaterial = new THREE.PointsMaterial({
-  color: new THREE.Color("#ff88cc"),
-  sizeAttenuation: true,
-  size: 0.4,
-  sizeAttenuation: true,
-  alphaMap: particleTexture,
-  alphaTest: 0.01,
-  // depthTest: false,
-  transparent: true,
-  depthWrite: false,
-  blending: THREE.AdditiveBlending,
-});
-
-// Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
-
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
-directionalLight.position.set(1, 1, 0);
-scene.add(directionalLight);
-
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Scroll
- */
-let scrollY = window.scrollY;
-let currentSection = 0;
-
-window.addEventListener("scroll", () => {
-  scrollY = window.scrollY;
-
-  const newSection = Math.round(scrollY / sizes.height);
-
-  if (newSection !== currentSection) {
-    currentSection = newSection;
-    gsap.to(sectionMeshes[currentSection].rotation, {
-      duration: 1.5,
-      ease: "power2.inOut",
-      x: "+=6",
-      y: "+=3",
-    });
-  }
-});
-
-/**
- * Cursor
- */
-const cursor = {};
-cursor.x = 0;
-cursor.y = 0;
-
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5;
-});
-
-/**
- * Animate
- */
-const clock = new THREE.Clock();
-let previousTime = 0;
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
-
-  // Animated camera
-  camera.position.y = (-scrollY / sizes.height) * objectsDistance;
-
-  const parallaxX = cursor.x * 0.5;
-  const parallaxY = -cursor.y * 0.5;
-  cameraGroup.position.x +=
-    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-  cameraGroup.position.y +=
-    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
-
-  // Animate meshed
-  for (const mesh of sectionMeshes) {
-    mesh.rotation.x += deltaTime * 0.1;
-    mesh.rotation.y += deltaTime * 0.12;
-  }
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
